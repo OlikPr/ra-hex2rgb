c9cbb2abcae5d2717cf24b74669fe7e7e6f66e0b
Olga Prihodko <prikhodkolik15@gmail.com>
2025-09-28 16:55:13 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { FormEvent, useState } from 'react'
 import './App.css'
-import Input  from './components/Input'
-
+import Input from './components/Input'
 
 function App() {
   const [state, setState] = useState('')
@@ -10,51 +9,62 @@ function App() {
     text: '',
     background: '',
     color: '',
-  });
+  })
 
-  const examString = (str : string) : boolean => {
-    return /^#[0-9, A, a, B, b, C, c, D, d, e, E, F, f]{6}/.test(str);
+  const examString = (str: string): boolean => {
+    return /^#[0-9A-Fa-f]{6}$/.test(str)
   } 
 
-  const toRgb = (hex: string) : string => {
-    const r = hex.substring(1,3);
-    const g = hex.substring(3,5);
-    const b = hex.substring(4);
-
-    return `rgb(${parseInt(r, 16)}, ${parseInt(g, 16)}, ${parseInt(b, 16)})`;
+  const toRgb = (hex: string): string => {
+    const r = hex.substring(1, 3)
+    const g = hex.substring(3, 5)
+    const b = hex.substring(5, 7)
+    return `rgb(${parseInt(r, 16)}, ${parseInt(g, 16)}, ${parseInt(b, 16)})`
   }
 
-  const debugInput = (event: FormEvent<HTMLInputElement>) : void => {
-    setState((event.target as HTMLInputElement).value)
+  const debugInput = (event: FormEvent<HTMLInputElement>): void => {
+    let inputValue = (event.target as HTMLInputElement).value
 
-    if ((event.target as HTMLInputElement).value.length === 7) {
-      if (examString((event.target as HTMLInputElement).value)) {
+
+    if (inputValue.length > 7) {
+      inputValue = inputValue.substring(0, 7)
+    }
+
+    setState(inputValue)
+
+    if (inputValue.length === 7) {
+      if (examString(inputValue)) {
         setConverter({
-          text: toRgb(state),
-          background: toRgb(state),
-          color: toRgb(state)
-        });
+          text: toRgb(inputValue),
+          background: inputValue,
+          color: '#000000'
+        })
       } else {
         setConverter({
-          text: 'Ошибка',
-          background: 'rgb(256, 0, 0)',
-          color: 'rgb(256, 0, 0)',
+          text: 'Ошибка!',
+          background: 'rgb(255, 0, 0)',
+          color: '#ffffff'
         })
       }
-    } 
+    } else {
+      setConverter({
+        text: '',
+        background: '',
+        color: ''
+      })
+    }
   }
 
   const container = {
-    background: converter.background,
+    backgroundColor: converter.background,
     color: converter.color
   }
 
   return (
     <div className='container' style={container}>
-        <Input value={state} placeholder={'Введите hex #ffffff'} onChange={debugInput}/>
-        <div className='converter' >{converter.text}</div>
-    </div>
-
+      <Input value={state} placeholder={'Введите hex #ffffff'} onChange={debugInput}/>
+      <div className='converter'>{converter.text}</div>
+      </div>
   )
 }
 
